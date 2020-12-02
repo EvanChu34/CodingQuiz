585df2ef79e4d6a953adaf29aee37beae1c6bd97
Evan Chu <echu34@gmail.com>
2020-12-02 16:54:29 -0500

    Some fixes from last night

--- script.js
+++ script.js
@@ -1,53 +1,76 @@
 var startBtn = document.querySelector("start-button");
 var answerBtn = document.createElement("");
 var answerChoices = document.createElement("li");
-var timerDisplay = document.querySelector("#seconds-left")
+var timerDisplay = document.querySelector("#seconds-left");
 var secondsLeft = 60;
 var test, question, choice, choices;
+var questionHead = doucment.createElement("p");
 var questionNumber = -1;
 var answer;
 var questions = [
     {
         title: "Which HTML element do we put in JavaScript?",
-        choices: ["JavaScript","script","js"]
+        choices: ["JavaScript","script","js"],
         answer: "script"
     },
     {
         title: "Where is the correct place to insert Javascript",
-        choices: ["In the head section", "in the body section", "Both A and B are right"]
+        choices: ["In the head section", "in the body section", "Both A and B are right"],
         answer: "Both A and B are right"
     },
     {
         title: "How do you write 'Hello World' in an alert box?",
-        choices: ["alert('Hello World');", "msgBox('Hello World');", "msg('Hello World');"]
+        choices: ["alert('Hello World');", "msgBox('Hello World');", "msg('Hello World');"],
         answer: "alert('Hello World');"
     },
     {
         title: "How do you write a function in JavaScript?",
-        choices: ["function:myFunction()", "function = myFunction()", "function myFunctions()"]
+        choices: ["function:myFunction()", "function = myFunction()", "function myFunctions()"],
         answer: "function myFunctions()"
     },
     {
         title: "Who created JavaScript?",
-        choices: ["Netscape","Samsung", "Elon Musk "]
+        choices: ["Netscape","Samsung", "Elon Musk "],
         answer: "Netscape"
     },
     {
         title: "How long did it take Brendan Eich to write the Javascript Langauge?",
-        choices: ["3 years", "10 days", "19 months"]
+        choices: ["3 years", "10 days", "19 months"],
         answer: "10 days"
     },
     {
         title: "is null an object?",
-        choices: ["True", "False", "Neither"]
+        choices: ["True", "False", "Neither"],
         answer: "True"
     },
     {
         title: "Java and Javascript are the same",
-        choices: ["Yes", "No", "Sometimes"]
+        choices: ["Yes", "No", "Sometimes"],
         answer: "No"
     }
 ];
+
+startBtn.addEventListener('click', startQuiz);
+
+function startQuiz(){
+    coundDown();
+    renderQuestion();
+    
+}
+
+function coundDown(){
+    var timeInterval = setInterval(function(){
+        if(secondsLeft <= 0){
+            clearInterval(secondsLeft = 0)
+        }
+        timerDisplay.innerHTML = secondsLeft
+        secondsLeft -=1
+    }, 1000)
+
+    if(timeInterval === 0){
+        renderResults();
+    }
+}
 
 function renderQuestion(){
     
@@ -68,23 +91,6 @@ function renderQuestion(){
 }
 
 
-function coundDown(){
-    var timeInterval = setInterval(function(){
-        if(secondsLeft <= 0){
-            clearInterval(secondsLeft = 0)
-        }
-        timerDisplay.innerHTML = secondsLeft
-        secondsLeft -=1
-    }, 1000)
-
-    if(timeInterval === 0){
-        renderResults();
-    }
-
-}
-
-startBtn.addEventListener('click', coundDown);
-startBtn.addEventListener("click", renderQuestion);
 
 function showResult(){
 	var answerResult = document.getElementByID("result")
