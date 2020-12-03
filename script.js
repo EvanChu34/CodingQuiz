0f2ef0cb7bf8a8e4050c207e3a1db44081ab3763
Evan Chu <echu34@gmail.com>
2020-12-02 20:03:04 -0500

    fixed index alittle and JS still not working

--- script.js
+++ script.js
@@ -1,86 +1,38 @@
 var startBtn = document.querySelector("start-button");
 var quizBody = document.querySelector("#quiz");
 
-var questionHeader = document.getElementById("questions")
-var answerChoices = document.getElementsById("answers");
+
 var timerDisplay = document.getElementById("timer");
 var secondsLeft = 60;
-var test, question, choice, choices;
-var questionNumber = 0;
+
+var questionHeader = document.getElementById("questions");
+var answerChoices = document.getElementsById("answers");
+var questionNumber = -1;
 var answer;
 
 
-var questions = [
-    {
-        title: "Which HTML element do we put in JavaScript?",
-        choices: ["JavaScript","script","js"],
-        answer: "script"
-    },
-    {
-        title: "Where is the correct place to insert Javascript",
-        choices: ["In the head section", "in the body section", "Both A and B are right"],
-        answer: "Both A and B are right"
-    },
-    {
-        title: "How do you write 'Hello World' in an alert box?",
-        choices: ["alert('Hello World');", "msgBox('Hello World');", "msg('Hello World');"],
-        answer: "alert('Hello World');"
-    },
-    {
-        title: "How do you write a function in JavaScript?",
-        choices: ["function:myFunction()", "function = myFunction()", "function myFunctions()"],
-        answer: "function myFunctions()"
-    },
-    {
-        title: "Who created JavaScript?",
-        choices: ["Netscape","Samsung", "Elon Musk "],
-        answer: "Netscape"
-    },
-    {
-        title: "How long did it take Brendan Eich to write the Javascript Langauge?",
-        choices: ["3 years", "10 days", "19 months"],
-        answer: "10 days"
-    },
-    {
-        title: "is null an object?",
-        choices: ["True", "False", "Neither"],
-        answer: "True"
-    },
-    {
-        title: "Java and Javascript are the same",
-        choices: ["Yes", "No", "Sometimes"],
-        answer: "No"
-    }
-];
-
-
-startBtn.addEventListener('click', startQuiz);
 
 function startQuiz(){
     coundDown();
+
     renderQuestion();
 }
 
 function coundDown(){
    var timerDown = setInterval(function(){
-        
         secondsLeft--;
         timerDisplay.textContent = "Time;" + secondsLeft;
 
-        if(secondsLeft <= 0){
+        if(secondsLeft === 0 || questionNumber === questions.length){
             clearInterval(timerDown)
         }
     }, 500)
-
-    //if(timeInterval === 0){
-    //    renderResults();
-    //}
 }
 
 function renderQuestion(){
 
     questionNumber++;
-	answer = questions[questionNumber].answer;
+	answer = questions[questionNumber].answer
 
 	questionHeader.textContent = questions[questionNumber].title;
 	answerChoices.innerHTML = "";
@@ -89,13 +41,14 @@ function renderQuestion(){
 
 	for (var i=0; i<choices.length; i++){
 		var nextQuestion = document.createElement("button");
-
 		nextQuestion.textContent = choices[i];
-        answerBtn = answerChoices.appendChild(nextQuestion)
-        
+        document.answerChoices.appendChild(nextQuestion)
     }
 
 }
+
+startBtn.addEventListener('click', renderQuestion);
+
 
 
 
